c5a349aa544047681584501d37ee2964b022abcc
amalinahamat <amalinahamat3107@gmail.com>
2024-07-02 21:27:08 +0800

    factorial number idesign

--- PythonBox/iModules.py
+++ PythonBox/iModules.py
@@ -111,6 +111,7 @@ change(s)
 '''
 
 # i design
+'''
 # 1
 
 number = int(input())
@@ -135,6 +136,18 @@ elif number > 2:
         print(n, end = " ")
 else:
     print("Invalid error")
+'''
+
+# 2
+number = int(input())
+
+i = 1
+total = 0
+for num in range(1,number + 1):
+    fact = num * i
+    total = total + fact
+
+print(total)
 
 
 
