4158a2a2a33603e51f559feafbbf59d7be3334ba
amalinahamat <amalinahamat3107@gmail.com>
2024-07-03 13:30:30 +0800

    inventory class with attributes assignment 7

--- PythonBox/ass7.py
+++ PythonBox/ass7.py
@@ -1,3 +1,4 @@
+'''
 # 1 
 # A pangram is a sentence using every letter of the alphabet at least once. It is case insensitive, 
 # so it doesn't matter if a letter is lower-case (e.g. k) or upper-case (e.g. K).
@@ -34,7 +35,9 @@ if alphabets <= list_char:
     print("The sentences is a PANGRAM")
 else:
     print("The sentences is NOT PANGRAM")
+'''
 
+'''
 # 2
 # n isogram (also known as a "non-pattern word") is a word or phrase without a repeating letter, 
 # however spaces and hyphens are allowed to appear multiple times.
@@ -63,6 +66,200 @@ if char_repeated:
 
 else:
     print(f"'{word}' is an isogram")
+'''
+'''
+
+# 3
+# Parse and evaluate simple math word problems returning the answer as an integer.
+print(first = int("What is 5?")f"   -> {}") 
+
+'''
+# 8
+# Write a Python class Inventory with attributes like id, productName, 
+# availableQuantity and price. Add methods like addItem, updateItem, 
+# and checkItem_details.
+# Use a dictionary to store the item details, where the key is the id 
+# and the value is a dictionary containing the productName, availableQuantity and price.
+'''
+class Inventory():
+
+    item_details = \
+        {
+            "97410": 
+                { 
+                    "productName" :"Television",
+                    "availableQuantity" : 20,
+                    "price" : 1455.99
+                },
+
+            "97411": 
+                { 
+                    "productName" :"Radio",
+                    "availableQuantity" : 32,
+                    "price" : 654.25
+                }           
+        }
+
+    def __init__ (self,id_number,productName,availableQuantity,price):
+        self.id_number = id_number
+        self.productName = productName
+        self.availableQuantity = availableQuantity
+        self.price = price
+
+    def addItem(self,id_number,productName,availableQuantity,price):
+        Inventory.item_details[id_number] = \
+        {
+            "productName" : productName,
+            "availableQuantity" : availableQuantity,
+            "price" : price
+        }
+        print(f"Item added: {productName}")
+
+    def updateItem(self,id_number,productName,availableQuantity,price):
+        if id_number in Inventory.item_details:
+            Inventory.item_details[id_number] = \
+            {
+                "productName" : productName,
+                "availableQuantity" : availableQuantity,
+                "price" : price
+            }
+            print(f"Item updated: {productName}")
+        else:
+            print(f"Item with ID {id_number} not found.")
+        
+
+    def checkItem_details(self,id_number):
+        if id_number in Inventory.item_details:
+            item = Inventory.item_details[id_number]
+            print(f"ID: {id_number}\nProduct Name: {item['productName']}\n"
+                  f"Available quantity: {item['availableQuantity']}\n"
+                  f"Price: {item['price']}")
+        else:
+            print(f"Item with ID {id_number} not found.")
+
+
+print("Select the matters:\n")
+print("1. Add item\n2. UpdateItem\n3. CheckItem\n")
+number = int(input("Enter the number: "))
+
+if number == 1:
+    id_number = input("Enter ID number: ")
+    productName = input("Enter product name: ")
+    availableQuantity = int(input("Enter available quantity: "))
+    price = float(input("Enter price of product: "))
+    Inventory.addItem(id_number,productName,availableQuantity,price)
+elif number == 2:
+    id_number = input("Enter ID number: ")
+    productName = input("Enter updated product name: ")
+    availableQuantity = int(input("Enter updated available quantity: "))
+    price = float(input("Enter updated price of product: "))
+    Inventory.updateItem(id_number,productName,availableQuantity,price)
+elif number == 3:
+    id_number = input("Enter ID number to be checked: ")
+    Inventory.checkItem_details(id_number)
+else:
+    print("Invalid input! Please enter just number between 1 and 3.")
+
+'''
+class Inventory():
+
+    item_details = \
+        {
+            "97410": 
+                { 
+                    "productName" : "Television",
+                    "availableQuantity" : 20,
+                    "price" : 1455.99
+                },
+
+            "97411": 
+                { 
+                    "productName" : "Radio",
+                    "availableQuantity" : 32,
+                    "price" : 654.25
+                }           
+        }
+
+    def __init__ (self,id_number,productName,availableQuantity,price):
+        self.id_number = id_number
+        self.productName = productName
+        self.availableQuantity = availableQuantity
+        self.price = price
+        #self.item_details = {}
+
+    def addItem(self,id_number,productName,availableQuantity,price):
+        self.item_details[id_number] = \
+        {
+            "productName" : productName,
+            "availableQuantity" : availableQuantity,
+            "price" : price
+        }
+        #print(f"Item added: {productName}")
+        print()
+        print(f"Item added:\n\nID: {id_number}\nProduct Name: {productName}\n"
+              f"Available quantity: {availableQuantity}\n"
+              f"Price: {price}")
+
+    def updateItem(self,id_number,productName,availableQuantity,price):
+        if id_number in self.item_details:
+            self.item_details[id_number] = \
+            {
+                "productName" : productName,
+                "availableQuantity" : availableQuantity,
+                "price" : price
+            }
+            #print(f"Item updated: {productName}")
+            print()
+            print(f"Item updated:\n\nID: {id_number}\nProduct Name: {productName}\n"
+                  f"Available quantity: {availableQuantity}\n"
+                  f"Price: {price}")
+        else:
+            print()
+            print(f"Item with ID {id_number} not found.")
+        
+
+    def checkItem_details(self,id_number):
+        if id_number in self.item_details:
+            item = self.item_details[id_number]
+            print()
+            print(f"ID: {id_number}\nProduct Name: {item['productName']}\n"
+                  f"Available quantity: {item['availableQuantity']}\n"
+                  f"Price: {item['price']}")
+        else:
+            print()
+            print(f"Item with ID {id_number} not found.")
+
+
+print("Select the matters:\n")
+print("1. Add item\n2. UpdateItem\n3. CheckItem\n")
+number = int(input("Enter the number: "))
+
+print()
+if number == 1:
+    id_number = input("Enter ID number: ")
+    productName = input("Enter product name: ")
+    availableQuantity = int(input("Enter available quantity: "))
+    price = float(input("Enter price of product: "))
+    inventory = Inventory(id_number, productName, availableQuantity, price)
+    inventory.addItem(id_number,productName,availableQuantity,price)
+elif number == 2:
+    id_number = input("Enter ID number: ")
+    productName = input("Enter updated product name: ")
+    availableQuantity = int(input("Enter updated available quantity: "))
+    price = float(input("Enter updated price of product: "))
+    inventory = Inventory(id_number, productName, availableQuantity, price)
+    inventory.updateItem(id_number,productName,availableQuantity,price)
+elif number == 3:
+    id_number = input("Enter ID number to be checked: ")
+    inventory = Inventory(id_number, "", 0, 0.0)
+    inventory.checkItem_details(id_number)
+else:
+    print("Invalid input! Please enter just number between 1 and 3.")
+
+
+
+
+
 
 
         
