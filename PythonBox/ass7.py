988ecb0bdb38c4e90a2b8b68f445debe1b3ff17d
amalinahamat <amalinahamat3107@gmail.com>
2024-07-03 11:01:13 +0800

    isogram problem assignment 7

--- PythonBox/ass7.py
+++ PythonBox/ass7.py
@@ -1,4 +1,10 @@
-# 1
+# 1 
+# A pangram is a sentence using every letter of the alphabet at least once. It is case insensitive, 
+# so it doesn't matter if a letter is lower-case (e.g. k) or upper-case (e.g. K).
+# For this exercise, a sentence is a pangram if it contains each of the 26 letters in the English alphabet.
+# Example: The quick brown fox jumps over the lazy dog.
+# Your task is to figure out if a sentence is a pangram.
+
 sentences = input("Write a sentences\n").strip()
 sentences = sentences.lower()
 sentences = sentences.replace(" ", "")
@@ -28,6 +34,35 @@ if alphabets <= list_char:
     print("The sentences is a PANGRAM")
 else:
     print("The sentences is NOT PANGRAM")
+
+# 2
+# n isogram (also known as a "non-pattern word") is a word or phrase without a repeating letter, 
+# however spaces and hyphens are allowed to appear multiple times.
+# Examples of isograms:
+# lumberjacks background downstream six-year-old
+# The word isograms, however, is not an isogram, because the s repeats.
+# Your task is to figure out if the user input is isogram
+
+word = input("Enter a word or phrase: ").strip()
+word = word.replace("-","")
+word = word.replace(".","")
+word = word.replace(",","")
+word = word.replace(" ","")
+
+char_set = set()
+char_repeated = []
+for char in word:
+    if char in char_set:
+        if char not in char_repeated:
+            char_repeated.append(char)      
+    else: 
+        char_set.add(char)
+
+if char_repeated:
+    print(f"'{word}' is not an isogram as {char_repeated} repeated")
+
+else:
+    print(f"'{word}' is an isogram")
 
 
         
