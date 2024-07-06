622a8a8586d3de0d27b7b1d3511ddc6ea27fa580
amalinahamat <amalinahamat3107@gmail.com>
2024-07-06 23:46:08 +0800

    validparentheses class ass7

--- PythonBox/ass7.py
+++ PythonBox/ass7.py
@@ -466,6 +466,7 @@ else:
 
 
 '''
+'''
 
 # 9
 from datetime import datetime
@@ -537,12 +538,40 @@ print("\nUpdated Account Details:")
 for account in bank_accounts:
     print(account)
 
-
+'''
 
 # 10
 
+class ValidParentheses:
+    def __init__(self):
+        self.open_list = ["[","{","("]
+        self.close_list = ["]","}",")"]
 
+    def check(self,Mystr):
+        stack = []
+        for i in Mystr:
+            if i in self.open_list:
+                stack.append(i)
+            elif i in self.close_list:
+                pos = self.close_list.index(i)
+                if ((len(stack) > 0) and (self.open_list[pos] == stack[len(stack) - 1])):
+                    stack.pop()
+                else:
+                    return "unbalanced"
+                
+        if len(stack) == 0:
+            return "Balanced"
+        else:
+            return "Unbalanced"
+        
+    
 
+checks = ValidParentheses()
+
+parentheses = input("Enter the parentheses: ").strip()
+
+result = checks.check(parentheses)
+print(f"The parentheses are {result}")
 
 
 
