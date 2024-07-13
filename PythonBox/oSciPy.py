27a753088232c58b207e412042875baf13bea4c6
amalinahamat <amalinahamat3107@gmail.com>
2024-07-13 12:23:19 +0800

    scipy

--- PythonBox/oSciPy.py
+++ PythonBox/oSciPy.py
@@ -3,14 +3,14 @@ import numpy as np
 from scipy.special import exp10, sindg, cosdg
 
 # Input the exponent value
-exponent = float(input("Enter the exponent value:\n"))
+exponent = int(input("Enter the exponent value: \n"))
     
 # Calculate 10 raised to the power of the exponent
 result_exp10 = exp10(exponent)
 print(f"10 raised to the power of {exponent} : {result_exp10}")
     
 # Input the angle in degrees
-angle_deg = float(input("Enter the angle in degrees:\n"))
+angle_deg = float(input("Enter the angle in degrees: \n"))
     
 # Calculate sine and cosine of the angle in degrees
 sine_value = sindg(angle_deg)
