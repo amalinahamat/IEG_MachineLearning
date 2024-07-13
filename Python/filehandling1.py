75efc3b26f6c8a47a15177dfc2b59399857db37d
amalinahamat <amalinahamat3107@gmail.com>
2024-07-13 17:57:02 +0800

    new file

--- Python/filehandling1.py
+++ Python/filehandling1.py
@@ -1,9 +1,11 @@
-import pickle
+import json
 
 try:
-    filehandler =  open('fruitsdictionary.txt', 'rt')
-    data = pickle.load(filehandler)
+    with open('fruitsdictionary.json') as filehandler:
+        data = json.load(filehandler) # data is the list of dictinary
     for product in data:
-        print(product)
+        print(product["product"])
+        print(product["quantity"])
+        print(product["price"])
 except Exception as e:
     print("Something went wrong")
