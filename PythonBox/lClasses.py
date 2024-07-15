34f8faed0cda6b3101d0be49a7f801b4b5682546
amalinahamat <amalinahamat3107@gmail.com>
2024-07-16 04:14:52 +0800

    IDESIGN

--- PythonBox/lClasses.py
+++ PythonBox/lClasses.py
@@ -608,6 +608,86 @@ class Person:
 
 # 4
 
+from City import City
+from State import State
+
+state_list = []
+state_list.append(State("Tamilnadu",[]))
+state_list.append(State("Andhra",[]))
+state_list.append(State("Karnataka",[]))
+state_list.append(State("Kerala",[]))
+
+print("Enter City Details")
+city_created_list = []
+choice = "Yes"
+while choice == "Yes" :
+	city_name = input("Enter city name\n")
+	state_name = input("Enter state name\n")
+	state_found_flag = 0
+	for state in state_list :
+		if state_name == state.name :
+			city = City(city_name, state)
+			state.city_list.append(city)
+			city_created_list.append(city)
+			state_found_flag = 1
+	if state_found_flag == 0 :
+		state = State(state_name,[])
+		state_list.append(state)
+		city = City(city_name, state)
+		city_created_list.append(city)
+		state.city_list.append(city)
+	choice = input("Do you want to add another city? Type Yes / No\n")
+
+print("\nCity Details\n")
+for city in city_created_list :
+	print(city)
+
+print("\nState Details\n")	
+for state in state_list :
+	print(state)
+
+
+class City:
+	
+	def __init__(self,name,state) :
+		self.__name = name
+		self.__state = state
+		
+	def __str__(self):
+		#Fill your code
+		return f"{self.__name} is in state {self.__state.name}"
+
+
+class State:
+	
+	def __init__(self,name,city_list) :
+		self.__name = name
+		self.__city_list = city_list
+		
+	@property
+	def city_list(self) :
+		#Fill your code
+		return self.__city_list
+	
+	@city_list.setter
+	def city_list(self,city_list) :
+		#Fill your code
+		self.__city_list = city_list
+
+	@property
+	def name(self) :
+		#Fill your code
+		return self.__name
+		
+		
+	def __str__(self) :
+		#city_names = ", ".join(city._City__name for city in self.__city_list)
+		city_count = len(self.__city_list)
+		return f"{self.__name} has {city_count} cities"
+	
+	
+	
+
 
 # 5
 '''
