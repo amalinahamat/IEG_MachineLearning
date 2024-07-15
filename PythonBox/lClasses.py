dae6f8048b7ceddcff6611cf97b3534bc4cb2216
amalinahamat <amalinahamat3107@gmail.com>
2024-07-16 04:12:10 +0800

    idesign

--- PythonBox/lClasses.py
+++ PythonBox/lClasses.py
@@ -564,6 +564,51 @@ person2 = Person.from_string(person_str)
 print("Person Details")
 print(person2)
 
+# 3
+
+from Person import Person
+from Address import Address
+
+person_name = input("Enter name\n")
+person_age = input("Enter age\n")
+print("Enter address")
+street = input("Enter street\n")
+city = input("Enter city\n")
+state = input("Enter state\n")
+person_address = Address(street,city,state)
+person = Person(person_name, person_age,person_address)
+print("Person Details")
+print(person)
+	
+class Address:
+	
+	def __init__(self,street, city, state):
+		self.__street = street
+		self.__city = city
+		self.__state = state
+	
+	def __str__(self):
+		#fill your code
+		return f"{self.__street} , {self.__city} , {self.__state}"		
+	
+		
+from Address import Address
+
+class Person:
+	
+	def __init__(self,name, age, address):
+		self.__name = name
+		self.__age = age
+		self.__address = address
+	
+	def __str__(self):
+		#fill your code
+		return f"{self.__name} is a person who is {self.__age} years old and lives in the following address : {self.__address}"
+		
+
+# 4
+
+
 # 5
 '''
 '''
